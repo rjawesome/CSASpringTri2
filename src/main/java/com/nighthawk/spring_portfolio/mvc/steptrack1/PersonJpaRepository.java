1772aa21dc25c3e40a435730a6a4809c299c42cb
Rohan Juneja <rohanj2006@gmail.com>
2022-11-22 11:38:18 -0800

    Create Person JPA repository

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/steptrack1/PersonJpaRepository.java
@@ -0,0 +1,16 @@
+package com.nighthawk.spring_portfolio.mvc.steptrack1;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+/*
+Extends the JpaRepository interface from Spring Data JPA.
+-- Java Persistent API (JPA) - Hibernate: map, store, update and retrieve database
+-- JpaRepository defines standard CRUD methods
+-- Via JPA the developer can retrieve database from relational databases to Java objects and vice versa.
+ */
+public interface PersonJpaRepository extends JpaRepository<Person, Long> {
+    Person findByEmail(String email);
+}
