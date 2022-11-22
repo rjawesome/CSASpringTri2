383103ec85c5bfbb52692dd301a33b85f728f4c0
Rohan Juneja <rohanj2006@gmail.com>
2022-11-22 11:32:34 -0800

    Create base day class

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/steptrack1/Day.java
@@ -0,0 +1,15 @@
+package com.nighthawk.spring_portfolio.mvc.steptrack1;
+
+import javax.persistence.*;
+
+@Entity
+public class Day {
+    // attributes, how would you track days.  perhaps capture date and have many method to calculate this data
+    private int year;
+    private int day;
+    private int steps;
+ 
+    // relationship
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Person person;
+}
