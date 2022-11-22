650b1ea89481008e8ec9d12d78a48fce2de57647
Rohan Juneja <rohanj2006@gmail.com>
2022-11-22 11:35:26 -0800

    Add lombok to person & other attributes

--- src/main/java/com/nighthawk/spring_portfolio/mvc/steptrack1/Person.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/steptrack1/Person.java
@@ -1,10 +1,16 @@
 package com.nighthawk.spring_portfolio.mvc.steptrack1;
 
 import javax.persistence.*;
+import lombok.*;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Person {
     // Person data
+    String name;
+    String email;
 
     // relationship
     @OneToMany(
