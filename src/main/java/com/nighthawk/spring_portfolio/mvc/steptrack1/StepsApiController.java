b70268e4e8ba63758df4c984b471569bc3382daf
rjawesome <aonedtop@gmail.com>
2022-11-22 18:49:17 -0800

    rename variable

--- src/main/java/com/nighthawk/spring_portfolio/mvc/steptrack1/StepsApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/steptrack1/StepsApiController.java
@@ -31,11 +31,11 @@ public class StepsApiController {
     GET individual Person using ID
      */
     @GetMapping("/getPerson")
-    public ResponseEntity<Object> getPerson(@RequestBody final Map<String,Object> stat_map) throws NoSuchAlgorithmException {
-        Optional<Person> optional = repository.findByEmail((String) stat_map.get("email"));
+    public ResponseEntity<Object> getPerson(@RequestBody final Map<String,Object> map) throws NoSuchAlgorithmException {
+        Optional<Person> optional = repository.findByEmail((String) map.get("email"));
         if (optional.isPresent()) {  // Good ID
             Person person = optional.get();  // value from findByID
-            String password = (String) stat_map.get("password");
+            String password = (String) map.get("password");
 
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] encodedHash = digest.digest(
@@ -73,16 +73,16 @@ public class StepsApiController {
     /*
     POST Aa record by Requesting Parameters from URI
      */
-    @PostMapping( "/createPerson")
-    public ResponseEntity<Object> postPerson(@RequestBody final Map<String,Object> stat_map) throws NoSuchAlgorithmException {
+    @PostMapping("/createPerson")
+    public ResponseEntity<Object> postPerson(@RequestBody final Map<String,Object> map) throws NoSuchAlgorithmException {
         // A person object WITHOUT ID will create a new record with default roles as student
         Person person = new Person();
-        person.setAge((int) stat_map.get("age"));
-        person.setName((String) stat_map.get("name"));
-        person.setEmail((String) stat_map.get("email"));
-        person.setGender((String) stat_map.get("gender"));
-        person.setHeightIn((int) stat_map.get("heightIn"));
-        person.setWeightLbs((int) stat_map.get("weightLbs"));
+        person.setAge((int) map.get("age"));
+        person.setName((String) map.get("name"));
+        person.setEmail((String) map.get("email"));
+        person.setGender((String) map.get("gender"));
+        person.setHeightIn((int) map.get("heightIn"));
+        person.setWeightLbs((int) map.get("weightLbs"));
 
 
         /* CHANGE TO A CALCULATION */
@@ -90,7 +90,7 @@ public class StepsApiController {
 
 
         // password hash
-        String password = (String) stat_map.get("password");
+        String password = (String) map.get("password");
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] encodedHash = digest.digest(
         password.getBytes(StandardCharsets.UTF_8));
@@ -104,12 +104,12 @@ public class StepsApiController {
     /*
     The personStats API adds stats by Date to Person table 
     */
-    @PostMapping(value = "/setStats", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> personStats(@RequestBody final Map<String,Object> stat_map) throws NoSuchAlgorithmException {
-        Optional<Person> optional = repository.findByEmail((String) stat_map.get("email"));
+    @PostMapping(value = "/setStats")
+    public ResponseEntity<Object> personStats(@RequestBody final Map<String,Object> map) throws NoSuchAlgorithmException {
+        Optional<Person> optional = repository.findByEmail((String) map.get("email"));
         if (optional.isPresent()) {  // Good Email
             Person person = optional.get();  // value from findByEmail
-            String password = (String) stat_map.get("password");
+            String password = (String) map.get("password");
 
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             byte[] encodedHash = digest.digest(
@@ -119,11 +119,11 @@ public class StepsApiController {
                 return new ResponseEntity<>("Incorrect Password", HttpStatus.BAD_REQUEST);        
             }
 
-            int dayInt = (int) stat_map.get("day");
-            int month = (int) stat_map.get("month");
-            int year = (int) stat_map.get("year");
-            int steps = (int) stat_map.get("steps");
-            int calories = (int) stat_map.get("calories");
+            int dayInt = (int) map.get("day");
+            int month = (int) map.get("month");
+            int year = (int) map.get("year");
+            int steps = (int) map.get("steps");
+            int calories = (int) map.get("calories");
             double miles = (double) steps / 2250;
 
             //check if the day already exists
