212498aa58c07b6446a894ed9a746f40f247ac8c
aaravarora01 <aaravarora09@gmail.com>
2022-11-24 16:17:40 -0800

    cleared errors

--- src/main/java/com/nighthawk/spring_portfolio/mvc/steptrack1/PersonJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/steptrack1/PersonJpaRepository.java
@@ -1,9 +1,7 @@
 package com.nighthawk.spring_portfolio.mvc.steptrack1;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 /*
