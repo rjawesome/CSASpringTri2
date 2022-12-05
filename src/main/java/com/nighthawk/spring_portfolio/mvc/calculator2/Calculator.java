ed1289fa390c04153be2e1c6128093c504969452
supermengman <andrewm17us@gmail.com>
2022-12-04 22:19:21 -0800

    lmao

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator2/Calculator.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator2/Calculator.java
@@ -47,22 +47,15 @@ public class Calculator {
         // original input
         this.expression = expression;
 
-        if (this.isBalanced(expression)) {
-            // parse expression into terms
-            this.termTokenizer();
+        
+        // parse expression into terms
+        this.termTokenizer();
 
-            this.tokensToReversePolishNotation();
+        this.tokensToReversePolishNotation();
 
-            // calculate reverse polish notation
-            this.rpnToResult();
-        } else {
-            try {
-                throw new BadParenthesisException();
-            } catch (BadParenthesisException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
+        // calculate reverse polish notation
+        this.rpnToResult();
+        
         
     }
 
@@ -84,11 +77,36 @@ public class Calculator {
         return (OPERATORS.get(token1) - OPERATORS.get(token2) >= 0) ;
     }
 
+    private boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '[' || c == '(' || c == '{' ) {     
+                stack.push(c);
+            } else if(c == ']') {
+                if(stack.isEmpty() || stack.pop() != '[') {
+                    return false;
+                }
+            } else if(c == ')') {
+                if(stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }           
+            } else if(c == '}') {
+                if(stack.isEmpty() || stack.pop() != '{') {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+    }
+
     // Term Tokenizer takes original expression and converts it to ArrayList of tokens
-    private void termTokenizer() {
-        
-        // contains final list of tokens
-        this.tokens = new ArrayList<>();
+    private void termTokenizer() throws BadParenthesisException {
+        if (this.isBalanced(expression)) {
+            // contains final list of tokens
+            this.tokens = new ArrayList<>();
 
             int start = 0;  // term split starting index
             StringBuilder multiCharTerm = new StringBuilder();    // term holder
@@ -121,6 +139,7 @@ public class Calculator {
         } else {
             throw new BadParenthesisException();
         }
+        
 
 
     }
@@ -139,12 +158,16 @@ public class Calculator {
                     tokenStack.push(token);
                     break;
                 case ")":
+                    
                     while (tokenStack.peek() != null && !tokenStack.peek().equals("("))
                     {
                         reverse_polish.add( tokenStack.pop() );
                     }
+                    
                     tokenStack.pop();
                     break;
+                    
+                    
                 case "+":
                 case "-":
                 case "*":
@@ -251,7 +274,7 @@ public class Calculator {
 
         System.out.println();
 
-        Calculator parenthesisMath = new Calculator("((100 + 200)  * 3");
+        Calculator parenthesisMath = new Calculator("(100 + 200)  * 3");
         System.out.println("Parenthesis Math\n" + parenthesisMath);
 
         System.out.println();
