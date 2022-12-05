9f1946871f6e44420bb820c09ed21b4ee9a500c9
Rohan Juneja <rohanj2006@gmail.com>
2022-12-04 22:05:33 -0800

    rework frq a bit

--- src/main/java/com/nighthawk/spring_portfolio/mvc/calculator1/DelimeterFRQ1.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/calculator1/DelimeterFRQ1.java
@@ -3,11 +3,17 @@ package com.nighthawk.spring_portfolio.mvc.calculator1;
 import java.util.ArrayList;
 
 public class DelimeterFRQ1 {
-    private String openDel;
-    private String closeDel;
-    public ArrayList<String> getDelimitersList(String[] tokens) {
+    private char openDel;
+    private char closeDel;
+    
+    public DelimeterFRQ1 (char o, char c) {
+        this.openDel = o;
+        this.closeDel = c;
+    }
+    
+    public ArrayList<String> getDelimitersList(String input) {
         ArrayList<String> delimeter = new ArrayList<String>();
-        for (String each : tokens) {
+        for (char each : input.toCharArray()) {
             if (each.equals(closeDel) || each.equals(openDel)) {
                 delimeter.add(each);
             }
