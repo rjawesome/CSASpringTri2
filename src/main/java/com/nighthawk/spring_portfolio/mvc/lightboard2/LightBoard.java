652023e4502173d95bd92af49b338753e4f6467a
horizon-ab <baileyasay@gmail.com>
2022-12-10 14:13:22 -0800

    checkerboard and frame patterns

--- src/main/java/com/nighthawk/spring_portfolio/mvc/lightboard2/LightBoard.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/lightboard2/LightBoard.java
@@ -11,28 +11,101 @@ public class LightBoard {
     boolean input = false;
 
     /* Initialize LightBoard and Lights */
-    public LightBoard(int numRows, int numCols, boolean input) {
+    public LightBoard(int numRows, int numCols, boolean input, String pattern) {
         this.lights = new Light[numRows][numCols];
         Scanner getInput = new Scanner(System.in);
-        String hexCode;
-        short effectInput;
         // 2D array nested loops, used for initialization
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
-                if (input) {
-                    System.out.println("Enter hexadecimal: ");
-                    hexCode = getInput.nextLine();
-                    System.out.println("Enter effect: ");
-                    effectInput = getInput.nextShort();
-                    getInput.nextLine(); // This line has to be added as buffer or else terminal will skip over next reading
-                    short[] rgbList = hexToRGB(hexCode);
-                    lights[row][col] = new Light(rgbList[0], rgbList[1], rgbList[2], effectInput);
-                } else {
-                    lights[row][col] = new Light(); // each cell needs to be constructed
-                }  
-            }
+        switch (pattern) {
+            case "checkerboard":
+                System.out.println("Enter hexadecimal 1: ");
+                String hexCode1 = getInput.nextLine();
+                System.out.println("Enter effect 1: ");
+                short effectInput1 = getInput.nextShort();
+                getInput.nextLine(); // This line has to be added as buffer or else terminal will skip over next reading
+                short[] rgbList1 = hexToRGB(hexCode1);
+                System.out.println("Enter hexadecimal 2: ");
+                String hexCode2 = getInput.nextLine();
+                System.out.println("Enter effect 2: ");
+                short effectInput2 = getInput.nextShort();
+                getInput.nextLine(); // This line has to be added as buffer or else terminal will skip over next reading
+                short[] rgbList2 = hexToRGB(hexCode2);
+
+                boolean alternate = true;
+                for (int row = 0; row < numRows; row++) {
+                    for (int col = 0; col < numCols; col++) {
+                        if (alternate) {
+                            lights[row][col] = new Light(rgbList1[0], rgbList1[1], rgbList1[2], effectInput1);
+                            if (numCols % 2 == 0 && col == numCols - 1) { // Skip over if at the end of an even number of columns otherwise the pattern won't match up
+                                continue;
+                            }
+                            alternate = false;
+                        } else {
+                            lights[row][col] = new Light(rgbList2[0], rgbList2[1], rgbList2[2], effectInput2);
+                            if (numCols % 2 == 0 && col == numCols - 1) { // Skip over if at the end of an even number of columns otherwise the pattern won't match up
+                                continue;
+                            }
+                            alternate = true;
+                        }
+                    }
+                }
+                break;
+            case "frame":
+                // Just to be clear, O stands for outer frame and I stands for inner picture
+                System.out.println("Enter hexadecimal 1: ");
+                String hexCodeO = getInput.nextLine();
+                System.out.println("Enter effect 1: ");
+                short effectInputO = getInput.nextShort();
+                getInput.nextLine(); // This line has to be added as buffer or else terminal will skip over next reading
+                short[] rgbListO = hexToRGB(hexCodeO);
+                System.out.println("Enter hexadecimal 2: ");
+                String hexCodeI = getInput.nextLine();
+                System.out.println("Enter effect 2: ");
+                short effectInputI = getInput.nextShort();
+                getInput.nextLine(); // This line has to be added as buffer or else terminal will skip over next reading
+                short[] rgbListI = hexToRGB(hexCodeI);
+
+                for (int row = 0; row < numRows; row++) {
+                    for (int col = 0; col < numCols; col++) {
+                        if (row == 0 || row == numRows - 1 || col == 0 || col == numCols - 1) {
+                            lights[row][col] = new Light(rgbListO[0], rgbListO[1], rgbListO[2], effectInputO);
+                        } else {
+                            lights[row][col] = new Light(rgbListI[0], rgbListI[1], rgbListI[2], effectInputI);
+                        }
+                    }
+                }
+                break;
+            default:
+                for (int row = 0; row < numRows; row++) {
+                    for (int col = 0; col < numCols; col++) {
+                        if (input) {
+                            System.out.println("Enter hexadecimal: ");
+                            String hexCode = getInput.nextLine();
+                            System.out.println("Enter effect: ");
+                            short effectInput = getInput.nextShort();
+                            getInput.nextLine(); // This line has to be added as buffer or else terminal will skip over next reading
+                            short[] rgbList = hexToRGB(hexCode);
+                            lights[row][col] = new Light(rgbList[0], rgbList[1], rgbList[2], effectInput);
+                        } else {
+                            lights[row][col] = new Light(); // each cell needs to be constructed
+                        }  
+                    }
+                }
         }
+        
         getInput.close();
+    }
+
+    // Many constructors for many different options
+    public LightBoard(int numRows, int numCols) {
+        this(numRows, numCols, false, "");
+    }
+
+    public LightBoard(int numRows, int numCols, boolean input) {
+        this(numRows, numCols, true, "");
+    }
+
+    public LightBoard(int numRows, int numCols, String pattern) {
+        this(numRows, numCols, true, pattern);
     }
 
     public boolean evaluateLight(int row, int col) {
@@ -51,9 +124,6 @@ public class LightBoard {
         return lights[row][col].effect == 0;
     }
 
-    public LightBoard(int numRows, int numCols) {
-        this(numRows, numCols, false);
-    }
     /* Output is intended for API key/values */
     public String toString() { 
         String outString = "[";
@@ -162,7 +232,7 @@ public class LightBoard {
     }
     static public void main(String[] args) {
         // create and display LightBoard
-        LightBoard lightBoard = new LightBoard(5, 5);
+        LightBoard lightBoard = new LightBoard(5, 5, "frame");
         System.out.println(lightBoard);  // use toString() method
         System.out.println(lightBoard.toTerminal());
         System.out.println(lightBoard.toColorPalette());
