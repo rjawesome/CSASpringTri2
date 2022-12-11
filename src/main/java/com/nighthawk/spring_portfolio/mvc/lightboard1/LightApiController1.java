eb5847df17d013e0b9eae49e5b15889bd3d6b5f3
Rohan Juneja <rohanj2006@gmail.com>
2022-12-10 18:51:55 -0800

    lightboard api

---
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/lightboard1/LightApiController1.java
@@ -0,0 +1,19 @@
+package com.nighthawk.spring_portfolio.mvc.lightboard1;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/light1")
+public class LightApiController1 {
+    @GetMapping("/getlights")
+    @CrossOrigin("*")
+    public static ResponseEntity<String> getLights () {
+        LightBoard1 lb = new LightBoard1(6, 6);
+        return new ResponseEntity<>(lb.toString(), HttpStatus.OK);
+    }
+}
