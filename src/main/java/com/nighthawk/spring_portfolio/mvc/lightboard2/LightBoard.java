b9636f431db7b4a033a9bcc4a1ef2267510fd627
horizon-ab <baileyasay@gmail.com>
2022-12-13 10:54:06 -0800

    commenting

--- src/main/java/com/nighthawk/spring_portfolio/mvc/lightboard2/LightBoard.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/lightboard2/LightBoard.java
@@ -77,6 +77,7 @@ public class LightBoard {
             default:
                 for (int row = 0; row < numRows; row++) {
                     for (int col = 0; col < numCols; col++) {
+                        // Getting input
                         if (input) {
                             System.out.println("Enter hexadecimal: ");
                             String hexCode = getInput.nextLine();
