7c90ddb5b0d1e014611527b64cfda468b794d0fa
Rohan Juneja <rohanj2006@gmail.com>
2023-01-08 21:42:27 -0800

    missing import

--- src/main/java/com/nighthawk/spring_portfolio/mvc/lightboard1/LightApiController1.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/lightboard1/LightApiController1.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/api/light1")
