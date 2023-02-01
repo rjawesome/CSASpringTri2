07020b6010007766821f5b86984f76093865bd68
horizon-ab <baileyasay@gmail.com>
2023-02-01 11:30:23 -0800

    flawed implementation for jwt api controller

--- src/main/java/com/nighthawk/spring_portfolio/mvc/login/JwtApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/login/JwtApiController.java
@@ -1,5 +1,51 @@
 package com.nighthawk.spring_portfolio.mvc.login;
 
+import java.util.Map;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.nighthawk.spring_portfolio.mvc.flashcards.User;
+
 public class JwtApiController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
     
+    @Autowired
+    private JwtUserDetailsService jwtUserDetailsService;
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody Map<String, String> authenticationRequest) throws Exception {
+        String email = (String) authenticationRequest.get("email");
+        String password = (String) authenticationRequest.get("password");
+        authenticate(email, password);
+        System.out.println("Good email and password");
+        final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(email);
+        final String token = jwtTokenUtil.generateToken(userDetails);
+        return null; //ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch(DisabledException e) {
+            System.out.println("Bad email and password");
+        } catch(BadCredentialsException e) {
+            System.out.println("Bad email and password");
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+    }
 }
