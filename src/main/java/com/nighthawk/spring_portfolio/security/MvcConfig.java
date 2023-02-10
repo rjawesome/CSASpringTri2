23ab399b9ffc71e2a2f8cc8a9fde775e72427c74
horizon-ab <baileyasay@gmail.com>
2023-02-10 10:21:34 -0800

    mvcconfig.java

---
+++ src/main/java/com/nighthawk/spring_portfolio/security/MvcConfig.java
@@ -0,0 +1,28 @@
+package com.nighthawk.spring_portfolio.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    // Set up index
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    // Map and path location for uploads outside of application resources
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/volumes/uploads/**").addResourceLocations("file:volumes/uploads/");
+    }
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://csa.rohanj.dev", "http://localhost:8085");
+    }
+}
