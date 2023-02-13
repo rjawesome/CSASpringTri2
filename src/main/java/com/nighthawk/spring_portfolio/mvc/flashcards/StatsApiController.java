4e120dee06cbd4920b67ea4c3c00359c3f719f0b
supermengman <andrewm17us@gmail.com>
2023-02-13 10:49:41 -0800

    set batch of stats

--- src/main/java/com/nighthawk/spring_portfolio/mvc/flashcards/StatsApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/flashcards/StatsApiController.java
@@ -13,144 +13,211 @@ import java.security.NoSuchAlgorithmException;
 @RestController
 @RequestMapping("/api/stats")
 public class StatsApiController {
-    /*
-    #### RESTful API ####
-    Resource: https://spring.io/guides/gs/rest-service/
-    */
+  /*
+   * #### RESTful API ####
+   * Resource: https://spring.io/guides/gs/rest-service/
+   */
 
-    // Autowired enables Control to connect POJO Object through JPA
-    @Autowired
-    private PersonJpaRepository repository;
+  // Autowired enables Control to connect POJO Object through JPA
+  @Autowired
+  private PersonJpaRepository repository;
 
-    @Autowired
-    private FlashcardSetJpaRepository flashcardSetRepository;
+  @Autowired
+  private FlashcardSetJpaRepository flashcardSetRepository;
 
-    @Autowired
-    private FlashcardJpaRepository flashcardRepository;
+  @Autowired
+  private FlashcardJpaRepository flashcardRepository;
 
-    @Autowired
-    private StatsJpaRepository statsRepository;
+  @Autowired
+  private StatsJpaRepository statsRepository;
 
-    @PostMapping("/getStatsByFlashcardSet")
-    public ResponseEntity<Object> getStatsByFlashcardSet(@RequestBody final Map<String,Object> map) throws NoSuchAlgorithmException {
-      Optional<Person> optional = repository.findByEmail((String) map.get("email"));
+  @PostMapping("/getStatsByFlashcardSet")
+  public ResponseEntity<Object> getStatsByFlashcardSet(@RequestBody final Map<String, Object> map)
+      throws NoSuchAlgorithmException {
+    Optional<Person> optional = repository.findByEmail((String) map.get("email"));
 
-      if (optional.isPresent()) {  // Good ID
-          Person person = optional.get();  // value from findByID
-          String password = (String) map.get("password");
+    if (optional.isPresent()) { // Good ID
+      Person person = optional.get(); // value from findByID
+      String password = (String) map.get("password");
 
-          MessageDigest digest = MessageDigest.getInstance("SHA-256");
-          byte[] encodedHash = digest.digest(
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] encodedHash = digest.digest(
           password.getBytes(StandardCharsets.UTF_8));
-          String computedPasswordHash = new String(encodedHash);
+      String computedPasswordHash = new String(encodedHash);
 
-          if (computedPasswordHash.equals(person.getPasswordHash())) {
-            // auth passed
-          }
-          else {
-            Map<String, Object> resp = new HashMap<>();
-            resp.put("err", "Incorrect Password");
-            return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);        
-          }
+      if (computedPasswordHash.equals(person.getPasswordHash())) {
+        // auth passed
       } else {
-          Map<String, Object> resp = new HashMap<>();
-          resp.put("err", "Incorrect Password");
-          return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);           
-       } 
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("err", "Incorrect Password");
+        return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+      }
+    } else {
+      Map<String, Object> resp = new HashMap<>();
+      resp.put("err", "Incorrect Password");
+      return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+    }
 
-       List<Stats> stats = statsRepository.findByUserAndFlashcardSet(optional.get(), flashcardSetRepository.findById((int) map.get("id")).get());
-       return new ResponseEntity<>(stats, HttpStatus.OK);
-    } 
+    List<Stats> stats = statsRepository.findByUserAndFlashcardSet(optional.get(),
+        flashcardSetRepository.findById((int) map.get("id")).get());
+    return new ResponseEntity<>(stats, HttpStatus.OK);
+  }
 
-    @PostMapping("/getStatsByFlashcard")
-    public ResponseEntity<Object> getStatsByFlashcard(@RequestBody final Map<String,Object> map) throws NoSuchAlgorithmException {
-      Optional<Person> optional = repository.findByEmail((String) map.get("email"));
+  @PostMapping("/getStatsByFlashcard")
+  public ResponseEntity<Object> getStatsByFlashcard(@RequestBody final Map<String, Object> map)
+      throws NoSuchAlgorithmException {
+    Optional<Person> optional = repository.findByEmail((String) map.get("email"));
 
-      if (optional.isPresent()) {  // Good ID
-          Person person = optional.get();  // value from findByID
-          String password = (String) map.get("password");
+    if (optional.isPresent()) { // Good ID
+      Person person = optional.get(); // value from findByID
+      String password = (String) map.get("password");
 
-          MessageDigest digest = MessageDigest.getInstance("SHA-256");
-          byte[] encodedHash = digest.digest(
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] encodedHash = digest.digest(
           password.getBytes(StandardCharsets.UTF_8));
-          String computedPasswordHash = new String(encodedHash);
+      String computedPasswordHash = new String(encodedHash);
 
-          if (computedPasswordHash.equals(person.getPasswordHash())) {
-            // auth passed
-          }
-          else {
-            Map<String, Object> resp = new HashMap<>();
-            resp.put("err", "Incorrect Password");
-            return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);        
-          }
+      if (computedPasswordHash.equals(person.getPasswordHash())) {
+        // auth passed
       } else {
-          Map<String, Object> resp = new HashMap<>();
-          resp.put("err", "Incorrect Password");
-          return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);           
-       } 
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("err", "Incorrect Password");
+        return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+      }
+    } else {
+      Map<String, Object> resp = new HashMap<>();
+      resp.put("err", "Incorrect Password");
+      return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+    }
 
-       List<Stats> stats = statsRepository.findByUserAndFlashcard(optional.get(), flashcardRepository.findById((int) map.get("id")).get());
-       return new ResponseEntity<>(stats, HttpStatus.OK);
-    } 
-  
-    @PostMapping("/createStats")
-    public ResponseEntity<Object> createStats(@RequestBody final Map<String,Object> map) throws NoSuchAlgorithmException {
-      Optional<Person> optional = repository.findByEmail((String) map.get("email"));
+    List<Stats> stats = statsRepository.findByUserAndFlashcard(optional.get(),
+        flashcardRepository.findById((int) map.get("id")).get());
+    return new ResponseEntity<>(stats, HttpStatus.OK);
+  }
 
-      if (optional.isPresent()) {  // Good ID
-          Person person = optional.get();  // value from findByID
-          String password = (String) map.get("password");
+  @PostMapping("/createStats")
+  public ResponseEntity<Object> createStats(@RequestBody final Map<String, Object> map)
+      throws NoSuchAlgorithmException {
+    Optional<Person> optional = repository.findByEmail((String) map.get("email"));
 
-          MessageDigest digest = MessageDigest.getInstance("SHA-256");
-          byte[] encodedHash = digest.digest(
+    if (optional.isPresent()) { // Good ID
+      Person person = optional.get(); // value from findByID
+      String password = (String) map.get("password");
+
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] encodedHash = digest.digest(
           password.getBytes(StandardCharsets.UTF_8));
-          String computedPasswordHash = new String(encodedHash);
+      String computedPasswordHash = new String(encodedHash);
 
-          if (computedPasswordHash.equals(person.getPasswordHash())) {
-            // auth passed
-          }
-          else {
-            Map<String, Object> resp = new HashMap<>();
-            resp.put("err", "Incorrect Password");
-            return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);        
-          }
+      if (computedPasswordHash.equals(person.getPasswordHash())) {
+        // auth passed
       } else {
-          Map<String, Object> resp = new HashMap<>();
-          resp.put("err", "Incorrect Password");
-          return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);           
-       } 
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("err", "Incorrect Password");
+        return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+      }
+    } else {
+      Map<String, Object> resp = new HashMap<>();
+      resp.put("err", "Incorrect Password");
+      return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+    }
 
-       Optional<Flashcard> flash = flashcardRepository.findById((int) map.get("id"));
-       if (!flash.isPresent()) {
+    Optional<Flashcard> flash = flashcardRepository.findById((int) map.get("id"));
+    if (!flash.isPresent()) {
+      Map<String, Object> resp = new HashMap<>();
+      resp.put("err", "Flashcard doesn't exist");
+      return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+    }
+
+    List<Stats> stats = statsRepository.findByUserAndFlashcard(optional.get(), flash.get());
+    if (stats.isEmpty()) {
+      Stats newStats = new Stats();
+      newStats.setUser(optional.get());
+      newStats.setFlashcard(flash.get());
+      newStats.setFlashcardSet(flash.get().getFlashcardSet());
+      if ((boolean) map.get("correct"))
+        stats.get(0).setCorrect(1);
+      else
+        stats.get(0).setIncorrect(1);
+      statsRepository.save(newStats);
+      return new ResponseEntity<>(newStats, HttpStatus.OK);
+    } else {
+      if ((boolean) map.get("correct"))
+        stats.get(0).setCorrect(stats.get(0).getCorrect() + 1);
+      else
+        stats.get(0).setIncorrect(stats.get(0).getIncorrect() + 1);
+      statsRepository.save(stats.get(0));
+      return new ResponseEntity<>(stats.get(0), HttpStatus.OK);
+    }
+
+  }
+
+  @PostMapping("/createStatsBatch")
+  public ResponseEntity<Object> createStatsBatch(@RequestBody final Map<String, Object> map)
+      throws NoSuchAlgorithmException {
+    Optional<Person> optional = repository.findByEmail((String) map.get("email"));
+
+    if (optional.isPresent()) { // Good ID
+      Person person = optional.get(); // value from findByID
+      String password = (String) map.get("password");
+
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] encodedHash = digest.digest(
+          password.getBytes(StandardCharsets.UTF_8));
+      String computedPasswordHash = new String(encodedHash);
+
+      if (computedPasswordHash.equals(person.getPasswordHash())) {
+        // auth passed
+      } else {
+        Map<String, Object> resp = new HashMap<>();
+        resp.put("err", "Incorrect Password");
+        return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+      }
+    } else {
+      Map<String, Object> resp = new HashMap<>();
+      resp.put("err", "Incorrect Password");
+      return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+    }
+
+    List<Map<String, Object>> statsList = (List<Map<String, Object>>) map.get("statsList");
+
+    for (var stat : statsList) {
+      Optional<Flashcard> flash = flashcardRepository.findById((int) stat.get("id"));
+      if (!flash.isPresent()) {
         Map<String, Object> resp = new HashMap<>();
         resp.put("err", "Flashcard doesn't exist");
         return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
-       }
+      }
 
-       List<Stats> stats = statsRepository.findByUserAndFlashcard(optional.get(), flash.get());
-       if (stats.isEmpty()) {
-          Stats newStats = new Stats();
-          newStats.setUser(optional.get());
-          newStats.setFlashcard(flash.get());
-          newStats.setFlashcardSet(flash.get().getFlashcardSet());
-          if ((boolean) map.get("correct")) stats.get(0).setCorrect(1);
-          else stats.get(0).setIncorrect(1);
-          statsRepository.save(newStats);
-          return new ResponseEntity<>(newStats, HttpStatus.OK);
-       } else {
-          if ((boolean) map.get("correct")) stats.get(0).setCorrect(stats.get(0).getCorrect() + 1);
-          else stats.get(0).setIncorrect(stats.get(0).getIncorrect() + 1);
-          statsRepository.save(stats.get(0));
-          return new ResponseEntity<>(stats.get(0), HttpStatus.OK);
-       }
-
-    } 
-
-    // handles exceptions
-    @ExceptionHandler({ClassCastException.class, NullPointerException.class})
-    public ResponseEntity<Object> handleBadUserInput () {
-      Map<String, Object> resp = new HashMap<>();
-      resp.put("err", "Bad user input");
-      return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+      List<Stats> stats = statsRepository.findByUserAndFlashcard(optional.get(), flash.get());
+      if (stats.isEmpty()) {
+        Stats newStats = new Stats();
+        newStats.setUser(optional.get());
+        newStats.setFlashcard(flash.get());
+        newStats.setFlashcardSet(flash.get().getFlashcardSet());
+        if ((boolean) stat.get("correct"))
+          stats.get(0).setCorrect(1);
+        else
+          stats.get(0).setIncorrect(1);
+        statsRepository.save(newStats);
+      } else {
+        if ((boolean) stat.get("correct"))
+          stats.get(0).setCorrect(stats.get(0).getCorrect() + 1);
+        else
+          stats.get(0).setIncorrect(stats.get(0).getIncorrect() + 1);
+        statsRepository.save(stats.get(0));
+      }
     }
+    Map<String, Object> resp = new HashMap<>();
+    resp.put("err", false);
+    return new ResponseEntity<>(resp, HttpStatus.OK);
+  }
+
+  // handles exceptions
+  @ExceptionHandler({ ClassCastException.class, NullPointerException.class })
+  public ResponseEntity<Object> handleBadUserInput() {
+    Map<String, Object> resp = new HashMap<>();
+    resp.put("err", "Bad user input");
+    return new ResponseEntity<>(resp, HttpStatus.BAD_REQUEST);
+  }
 }
