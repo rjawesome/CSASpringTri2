ea7ac334d9d88989806fb40172c012d03b81859a
Rohan Juneja <rohanj2006@gmail.com>
2023-02-14 10:26:40 -0800

    stats bug fix

--- src/main/java/com/nighthawk/spring_portfolio/mvc/flashcards/StatsApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/flashcards/StatsApiController.java
@@ -136,9 +136,9 @@ public class StatsApiController {
       newStats.setFlashcard(flash.get());
       newStats.setFlashcardSet(flash.get().getFlashcardSet());
       if ((boolean) map.get("correct"))
-        stats.get(0).setCorrect(1);
+        newStats.setCorrect(1);
       else
-        stats.get(0).setIncorrect(1);
+        newStats.setIncorrect(1);
       statsRepository.save(newStats);
       return new ResponseEntity<>(newStats, HttpStatus.OK);
     } else {
