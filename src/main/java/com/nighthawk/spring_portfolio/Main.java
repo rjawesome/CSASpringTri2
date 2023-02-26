4bddbaf5752b8c7d80c787c0f15e611e6e0640e9
Rohan Juneja <rohanj2006@gmail.com>
2023-02-25 16:29:11 -0800

    import event listener

--- src/main/java/com/nighthawk/spring_portfolio/Main.java
+++ src/main/java/com/nighthawk/spring_portfolio/Main.java
@@ -3,6 +3,7 @@ package com.nighthawk.spring_portfolio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 // @SpringBootApplication annotation is key to building web applications using Java https://spring.io/projects/spring-boot
 @SpringBootApplication
