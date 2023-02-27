e0a168cbbde3a4f5d377848ae98ba6d5abded11d
horizon-ab <baileyasay@gmail.com>
2023-02-27 09:20:54 -0800

    only admin can call getDB

--- src/main/java/com/nighthawk/spring_portfolio/mvc/flashcards/DatabaseApiController.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/flashcards/DatabaseApiController.java
@@ -3,12 +3,17 @@ package com.nighthawk.spring_portfolio.mvc.flashcards;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,22 +22,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/db")
 public class DatabaseApiController {
 
+  @Autowired
+  LoginHandler handler;
+
   @GetMapping("/sqlite.db")
-  public ResponseEntity<Object> getDB () throws IOException {
-    Resource coolDbResource = new FileSystemResource("volumes/sqlite.db");
+  public ResponseEntity<Object> getDB (@CookieValue("flashjwt") String jwt) throws IOException {
+    Person p = handler.decodeJwt(jwt);
+    if (p.isAdmin()) {
+      Resource coolDbResource = new FileSystemResource("volumes/sqlite.db");
 
-    // Check if the file exists
-    if (!coolDbResource.exists()) {
-        throw new FileNotFoundException("sqlite.db file not found");
+      // Check if the file exists
+      if (!coolDbResource.exists()) {
+          throw new FileNotFoundException("sqlite.db file not found");
+      }
+
+      // Set the content type of the file
+      String contentType = "application/octet-stream";
+
+      // Returns the response back from the database
+      return ResponseEntity.ok()
+              .contentType(MediaType.parseMediaType(contentType))
+              .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + coolDbResource.getFilename() + "\"")
+              .body(coolDbResource);
+    } else {
+      Map<String, Object> resp = new HashMap<>();
+      resp.put("err", "Unauthorized");
+      return new ResponseEntity<>(resp, HttpStatus.UNAUTHORIZED);
     }
-
-    // Set the content type of the file
-    String contentType = "application/octet-stream";
-
-    // Returns the response back from the database
-    return ResponseEntity.ok()
-            .contentType(MediaType.parseMediaType(contentType))
-            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + coolDbResource.getFilename() + "\"")
-            .body(coolDbResource);
+      
   }
 }
