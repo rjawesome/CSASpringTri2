4160e2f0b64a5e3b5ca05f1b22855cf618d221fb
nicm2 <nmounier@live.com>
2023-02-28 09:52:52 -0800

    code

--- src/main/java/com/nighthawk/spring_portfolio/mvc/flashcards/FlashcardJpaRepository.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/flashcards/FlashcardJpaRepository.java
@@ -13,3 +13,10 @@ public interface FlashcardJpaRepository extends JpaRepository<Flashcard, Long> {
   Optional<Flashcard> findById(long id);
   List<Flashcard> findByFlashcardSet(FlashcardSet flashcardSet);
 }
+
+/*
+Extends the JpaRepository interface from Spring Data JPA.
+-- Java Persistent API (JPA) - Hibernate: map, store, update and retrieve database
+-- JpaRepository defines standard CRUD methods
+-- Via JPA the developer can retrieve database from relational databases to Java objects and vice versa.
+ */
